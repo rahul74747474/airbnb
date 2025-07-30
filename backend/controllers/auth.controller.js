372714c6c39dd3e4d6174baa6686f5a6a4caaa8e
rahul74747474 <ghibli141525@gmail.com>
2025-07-30 11:16:02 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -39,13 +39,14 @@ export const login = async (req, res) => {
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
     res
-      .cookie("token", token, {
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      })
-      .json({ user: { _id: user._id, name: user.name, email: user.email } });
+  .cookie("token", token, {
+    httpOnly: true,
+    sameSite: "none", // allows cross-site cookies
+    secure: true,     // required with SameSite: 'none'
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  })
+  .json({ user: { _id: user._id, name: user.name, email: user.email } });
+
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
