e9adf175edc64176f367de924404e6d70a50b978
rahul74747474 <ghibli141525@gmail.com>
2025-07-30 09:35:01 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Make sure this matches your backend route
+  baseURL: "https://airbnb-znta.onrender.com/api", // Make sure this matches your backend route
   withCredentials: true, // Only if you're using cookies (optional)
 });
 
